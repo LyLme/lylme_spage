4ca5a754ad613c72840e48881e47a5f9830f3471
LyLme <admin@lylme.com>
2022-01-20 15:38:11 +0800

    项目php文件

---
+++ include/common.php
@@ -0,0 +1,15 @@
+<?php
+include_once 'config.php';
+if(!$con =mysqli_connect($dbconfig['host'],$dbconfig['user'],$dbconfig['pwd'],$dbconfig['dbname'],$dbconfig['port'])) {
+	if(mysqli_connect_errno()==2002) {
+		echo '连接数据库失败，数据库地址填写错误！';
+	} elseif(mysqli_connect_errno()==1045) {
+		echo '连接数据库失败，数据库用户名或密码填写错误！';
+	} elseif(mysqli_connect_errno()==1049) {
+		echo '连接数据库失败，数据库名不存在！';
+	} else {
+		echo '连接数据库失败，['.mysqli_connect_errno().']'.mysqli_connect_error();
+	}
+}
+?>
+
