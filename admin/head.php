6328dd8d22dd863e27da8a1e4e9943e017fa11dc
LyLme <admin@lylme.com>
2022-02-22 03:09:48 +0800

    删除无效功能

--- admin/head.php
+++ admin/head.php
@@ -8,23 +8,13 @@ include_once("../include/common.php");
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
-<title><?php echo $title;?> - 六零(LyLme Spage)</title>
+<title><?php echo $title.' - '.$conf['title'];?></title>
 <link rel="icon" href="/assets/img/logo.png" type="image/ico">
 <meta name="author" content="yinqi">
 <link href="css/bootstrap.min.css" rel="stylesheet">
 <link href="css/materialdesignicons.min.css" rel="stylesheet">
 <link href="css/style.min.css" rel="stylesheet">
 </head>
- <script language="javascript">
-function loginout(){
-if( confirm("你确实要退出吗？")){
-window.parent.location.href="login.php?logout";
-}
-else{
-return;
-}
-}
-</script>   
 <body>
 <div class="lyear-layout-web">
   <div class="lyear-layout-container">
@@ -33,7 +23,7 @@ return;
       
       <!-- logo -->
       <div id="logo" class="sidebar-header">
-        <a href="./"><img src="/assets/img/logo-sidebar.png" title="LyLme" alt="LyLme" /></a>
+        <a href="./"><img src="/assets/img/logo-sidebar.png"  alt="LyLme" /></a>
       </div>
       <div class="lyear-layout-sidebar-scroll"> 
         
@@ -44,8 +34,7 @@ return;
               <a href="javascript:void(0)"><i class="mdi mdi-palette"></i>网站配置</a>
               <ul class="nav nav-subnav">
                 <li> <a href="./set.php">基本设置</a> </li>
-                <li> <a href="./set_sou.php">搜索设置</a> </li>
-                <li> <a href="./set_plu.php">插件设置</a> </li>
+                <!--<li> <a href="./set_sou.php">搜索设置</a> </li>-->
                 <li> <a href="./user.php">修改密码</a> </li>
               </ul>
             </li>
@@ -70,7 +59,7 @@ return;
         </nav>
         
         <div class="sidebar-footer">
-          <p class="copyright">Copyright ©2021 - <?php echo(date('Y'));  ?> <a href="/">LyLme Spage</a>.</p>
+          <p class="copyright">Copyright &copy;<?php echo(date('Y'));  ?> By LyLme.<br> <a href="https://gitee.com/LyLme/lylme_spage"><?php echo $conf['title'];?> </a></p>
         </div>
       </div>
       
@@ -103,133 +92,6 @@ return;
                 <li> <a href="javascript:loginout()"><i class="mdi mdi-logout-variant"></i> 退出登录</a> </li>
               </ul>
             </li>
-            <!--切换主题配色-->
-		    <li class="dropdown dropdown-skin">
-			  <span data-toggle="dropdown" class="icon-palette"><i class="mdi mdi-palette"></i></span>
-			  <ul class="dropdown-menu dropdown-menu-right" data-stopPropagation="true">
-                <li class="drop-title"><p>主题</p></li>
-                <li class="drop-skin-li clearfix">
-                  <span class="inverse">
-                    <input type="radio" name="site_theme" value="default" id="site_theme_1" checked>
-                    <label for="site_theme_1"></label>
-                  </span>
-                  <span>
-                    <input type="radio" name="site_theme" value="dark" id="site_theme_2">
-                    <label for="site_theme_2"></label>
-                  </span>
-                  <span>
-                    <input type="radio" name="site_theme" value="translucent" id="site_theme_3">
-                    <label for="site_theme_3"></label>
-                  </span>
-                </li>
-			    <li class="drop-title"><p>LOGO</p></li>
-				<li class="drop-skin-li clearfix">
-                  <span class="inverse">
-                    <input type="radio" name="logo_bg" value="default" id="logo_bg_1" checked>
-                    <label for="logo_bg_1"></label>
-                  </span>
-                  <span>
-                    <input type="radio" name="logo_bg" value="color_2" id="logo_bg_2">
-                    <label for="logo_bg_2"></label>
-                  </span>
-                  <span>
-                    <input type="radio" name="logo_bg" value="color_3" id="logo_bg_3">
-                    <label for="logo_bg_3"></label>
-                  </span>
-                  <span>
-                    <input type="radio" name="logo_bg" value="color_4" id="logo_bg_4">
-                    <label for="logo_bg_4"></label>
-                  </span>
-                  <span>
-                    <input type="radio" name="logo_bg" value="color_5" id="logo_bg_5">
-                    <label for="logo_bg_5"></label>
-                  </span>
-                  <span>
-                    <input type="radio" name="logo_bg" value="color_6" id="logo_bg_6">
-                    <label for="logo_bg_6"></label>
-                  </span>
-                  <span>
-                    <input type="radio" name="logo_bg" value="color_7" id="logo_bg_7">
-                    <label for="logo_bg_7"></label>
-                  </span>
-                  <span>
-                    <input type="radio" name="logo_bg" value="color_8" id="logo_bg_8">
-                    <label for="logo_bg_8"></label>
-                  </span>
-				</li>
-				<li class="drop-title"><p>头部</p></li>
-				<li class="drop-skin-li clearfix">
-                  <span class="inverse">
-                    <input type="radio" name="header_bg" value="default" id="header_bg_1" checked>
-                    <label for="header_bg_1"></label>                      
-                  </span>                                                    
-                  <span>                                                     
-                    <input type="radio" name="header_bg" value="color_2" id="header_bg_2">
-                    <label for="header_bg_2"></label>                      
-                  </span>                                                    
-                  <span>                                                     
-                    <input type="radio" name="header_bg" value="color_3" id="header_bg_3">
-                    <label for="header_bg_3"></label>
-                  </span>
-                  <span>
-                    <input type="radio" name="header_bg" value="color_4" id="header_bg_4">
-                    <label for="header_bg_4"></label>                      
-                  </span>                                                    
-                  <span>                                                     
-                    <input type="radio" name="header_bg" value="color_5" id="header_bg_5">
-                    <label for="header_bg_5"></label>                      
-                  </span>                                                    
-                  <span>                                                     
-                    <input type="radio" name="header_bg" value="color_6" id="header_bg_6">
-                    <label for="header_bg_6"></label>                      
-                  </span>                                                    
-                  <span>                                                     
-                    <input type="radio" name="header_bg" value="color_7" id="header_bg_7">
-                    <label for="header_bg_7"></label>
-                  </span>
-                  <span>
-                    <input type="radio" name="header_bg" value="color_8" id="header_bg_8">
-                    <label for="header_bg_8"></label>
-                  </span>
-				</li>
-				<li class="drop-title"><p>侧边栏</p></li>
-				<li class="drop-skin-li clearfix">
-                  <span class="inverse">
-                    <input type="radio" name="sidebar_bg" value="default" id="sidebar_bg_1" checked>
-                    <label for="sidebar_bg_1"></label>
-                  </span>
-                  <span>
-                    <input type="radio" name="sidebar_bg" value="color_2" id="sidebar_bg_2">
-                    <label for="sidebar_bg_2"></label>
-                  </span>
-                  <span>
-                    <input type="radio" name="sidebar_bg" value="color_3" id="sidebar_bg_3">
-                    <label for="sidebar_bg_3"></label>
-                  </span>
-                  <span>
-                    <input type="radio" name="sidebar_bg" value="color_4" id="sidebar_bg_4">
-                    <label for="sidebar_bg_4"></label>
-                  </span>
-                  <span>
-                    <input type="radio" name="sidebar_bg" value="color_5" id="sidebar_bg_5">
-                    <label for="sidebar_bg_5"></label>
-                  </span>
-                  <span>
-                    <input type="radio" name="sidebar_bg" value="color_6" id="sidebar_bg_6">
-                    <label for="sidebar_bg_6"></label>
-                  </span>
-                  <span>
-                    <input type="radio" name="sidebar_bg" value="color_7" id="sidebar_bg_7">
-                    <label for="sidebar_bg_7"></label>
-                  </span>
-                  <span>
-                    <input type="radio" name="sidebar_bg" value="color_8" id="sidebar_bg_8">
-                    <label for="sidebar_bg_8"></label>
-                  </span>
-				</li>
-			  </ul>
-			</li>
-            <!--切换主题配色-->
           </ul>
           
         </div>
