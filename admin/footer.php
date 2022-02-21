35d8c663078ec900f4f5db2f9703848bd636e800
LyLme <admin@lylme.com>
2022-02-22 05:23:33 +0800

    删除多余符号

--- admin/footer.php
+++ admin/footer.php
@@ -2,5 +2,5 @@
 <script type="text/javascript" src="js/bootstrap.min.js"></script>
 <script type="text/javascript" src="js/perfect-scrollbar.min.js"></script>
 <script type="text/javascript" src="js/main.min.js"></script>
-</body>
-</html><
+    </body>
+</html>
