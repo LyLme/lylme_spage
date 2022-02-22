409d0ee4c4ad331f59795721f45c6ee072e7de45
LyLme <admin@lylme.com>
2022-02-22 16:25:14 +0800

    修复后台无法推出

--- admin/footer.php
+++ admin/footer.php
@@ -1,6 +1,16 @@
+<script language="javascript">
+function loginout(){
+if( confirm("你确实要退出吗？")){
+window.parent.location.href="login.php?logout";
+}
+else{
+return;
+}
+}
+</script>
 <script type="text/javascript" src="js/jquery.min.js"></script>
 <script type="text/javascript" src="js/bootstrap.min.js"></script>
 <script type="text/javascript" src="js/perfect-scrollbar.min.js"></script>
 <script type="text/javascript" src="js/main.min.js"></script>
-    </body>
+</body>
 </html>
