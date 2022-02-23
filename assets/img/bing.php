adbddd8132f41abf9cabb46ea25e0dcb73fbbdf6
LyLme <admin@lylme.com>
2022-02-23 22:58:08 +0800

    删除修饰符e

--- assets/img/bing.php
+++ assets/img/bing.php
@@ -6,7 +6,7 @@
  */
 
 $str=file_get_contents('http://cn.bing.com/HPImageArchive.aspx?idx=0&n=1');
-if(preg_match("/<url>(.+?)<\/url>/ies",$str,$matches)){
+if(preg_match("/<url>(.+?)<\/url>/is",$str,$matches)){
 $imgurl='http://cn.bing.com'.$matches[1];
 }
 if($imgurl){
