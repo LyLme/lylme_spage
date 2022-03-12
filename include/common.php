b829a6f42e808002752c062373211f906fbecb0e
LyLme <admin@lylme.com>
2022-03-12 15:23:32 +0800

    修改 数据库连接方式

--- include/common.php
+++ include/common.php
@@ -1,41 +1,28 @@
 <?php
 header ("Content-type:text/html;charset=utf-8");
-
+define('IN_CRONLITE', true);
 define('SYS_KEY', 'lylme_key');
 define('SYSTEM_ROOT', dirname(__FILE__).'/');
 define('ROOT', dirname(SYSTEM_ROOT).'/');
-
-include ROOT.'config.php';
-if(!$con =mysqli_connect($dbconfig['host'],$dbconfig['user'],$dbconfig['pwd'],$dbconfig['dbname'],$dbconfig['port'])) {
-	if(mysqli_connect_errno()==2002) {
-		echo '连接数据库失败，数据库地址填写错误！';
-	} elseif(mysqli_connect_errno()==1045) {
-		echo '连接数据库失败，数据库用户名或密码填写错误！';
-	} elseif(mysqli_connect_errno()==1049) {
-		echo '连接数据库失败，数据库名不存在！';
-	} else {
-		echo '连接数据库失败，['.mysqli_connect_errno().']'.mysqli_connect_error();
-	}
+require ROOT.'config.php';
+if(!defined('SQLITE') && (!$dbconfig['user']||!$dbconfig['pwd']||!$dbconfig['dbname']))//检测安装
+{
+@unlink(ROOT.'install/install.lock');
+header('Content-type:text/html;charset=utf-8');
+echo '你还没安装！<a href="install/">点此安装</a>';
+exit();
 }
-$GLOBALS['con'] = $con;
-mysqli_query($con,"set sql_mode = ''");
- //字符转换，读库
-mysqli_query($con,"set character set 'utf8'");
-//写库
-mysqli_query($con,"set names 'utf8'");
+include_once(SYSTEM_ROOT."db.class.php");
+$DB=new DB($dbconfig['host'],$dbconfig['user'],$dbconfig['pwd'],$dbconfig['dbname'],$dbconfig['port']);
 
-$rs= mysqli_query($con,"SELECT * FROM `lylme_config`");
+
+$rs= $DB->query("SELECT * FROM `lylme_config`");
 while($row = mysqli_fetch_assoc($rs)) { 
 	$conf[$row['k']]=$row['v'];
 }
-
-
 include_once(SYSTEM_ROOT."function.php");
 include_once(SYSTEM_ROOT."member.php");
 include_once(SYSTEM_ROOT."tj.php");
-mysqli_query($con,'set names utf8');
-$linksrows=mysqli_num_rows(mysqli_query($con, "SELECT * FROM `lylme_links`"));
-$groupsrows=mysqli_num_rows(mysqli_query($con, "SELECT * FROM `lylme_groups`"));
-
+$linksrows=$DB->num_rows($DB->query("SELECT * FROM `lylme_links`"));
+$groupsrows=$DB->num_rows($DB->query("SELECT * FROM `lylme_groups`"));
 ?>
-
