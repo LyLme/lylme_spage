21087b7d738d3f199a179270b7f41ef24fe39427
LyLme <admin@lylme.com>
2022-03-19 21:53:07 +0800

    修复错误

--- include/member.php
+++ include/member.php
@@ -1,10 +1,9 @@
 <?php 
-
 if(isset($_COOKIE["admin_token"]))
 {
 	$token=authcode(daddslashes($_COOKIE['admin_token']), 'DECODE', SYS_KEY);
 	list($user, $sid) = explode("\t", $token);
-	$session=md5($conf['admin_user'].$conf['admin_pwd'].$password_hash);
+	$session=md5($conf['admin_user'].$conf['admin_pwd']);
 	if($session==$sid) {
 		$islogin=1;
 	}
