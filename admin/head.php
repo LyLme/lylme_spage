d03d55e1cf15b6fc519d6f7a2e04f936a49eace7
LyLme <admin@lylme.com>
2022-03-26 16:06:03 +0800

    增加 收录管理入口

--- admin/head.php
+++ admin/head.php
@@ -1,14 +1,16 @@
 <?php
 include_once("../include/common.php");
-if(isset($islogin)==1){}else exit("<script language='javascript'>window.location.href='./login.php';</script>");
-$update = update(); //检查更新
+if(isset($islogin)==1) {
+} else exit("<script language='javascript'>window.location.href='./login.php';</script>");
+$update = update();
 ?>
 <!DOCTYPE html>
 <html lang="zh">
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
-<title><?php echo $title.' - '.$conf['title'];?></title>
+<title><?php echo $title.' - '.$conf['title'];
+?></title>
 <link rel="icon" href="/assets/img/logo.png" type="image/ico">
 <meta name="author" content="yinqi">
 <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -25,7 +27,6 @@ $update = update(); //检查更新
         <a href="/"><img src="/assets/img/logo-sidebar.png"  alt="LyLme" title="返回首页" /></a>
       </div>
       <div class="lyear-layout-sidebar-scroll"> 
-        
         <nav class="sidebar-main">
           <ul class="nav nav-drawer">
             <li class="nav-item active"> <a href="./"><i class="mdi mdi-home-map-marker"></i>后台首页</a> </li>
@@ -38,6 +39,7 @@ $update = update(); //检查更新
                 <li> <a href="./user.php">修改账号密码</a> </li>
               </ul>
             </li>
+            <li class="nav-item active"> <a href="./apply.php"><i class="mdi mdi-link"></i>收录管理</a> </li>
             <li class="nav-item nav-item-has-subnav">
               <a href="javascript:void(0)"><i class="mdi mdi-folder"></i> <span>分组管理</span></a>
               <ul class="nav nav-subnav">
@@ -48,7 +50,6 @@ $update = update(); //检查更新
             <li class="nav-item nav-item-has-subnav">
               <a href="javascript:void(0)"><i class="mdi mdi-web"></i> <span>链接管理</span></a>
               <ul class="nav nav-subnav">
-                  
                 <li> <a href="./link.php?set=add">添加链接</a> </li>
                 <li> <a href="./link.php">管理链接</a> </li>
               </ul>
@@ -57,34 +58,33 @@ $update = update(); //检查更新
              <li> <a href="javascript:loginout()"><i class="mdi mdi-logout"></i> 退出登录</a> </li>
              </ul>
         </nav>
-        
         <div class="sidebar-footer">
-          <p class="copyright">Copyright &copy;<?php echo(date('Y'));  ?> By LyLme.<br> <a href="https://gitee.com/LyLme/lylme_spage"><?php echo $conf['title'];?> </a></p>
+          <p class="copyright">Copyright &copy;
+<?php echo(date('Y'));
+?> By LyLme.<br> <a href="https://gitee.com/LyLme/lylme_spage"><?php echo $conf['title'];
+?> </a></p>
         </div>
       </div>
-      
     </aside>
     <!--End 左侧导航-->
-    
     <!--头部信息-->
     <header class="lyear-layout-header">
-      
       <nav class="navbar navbar-default">
         <div class="topbar">
-          
           <div class="topbar-left">
             <div class="lyear-aside-toggler">
               <span class="lyear-toggler-bar"></span>
               <span class="lyear-toggler-bar"></span>
               <span class="lyear-toggler-bar"></span>
             </div>
-            <span class="navbar-page-title"> <?php echo $title;?></span>
+            <span class="navbar-page-title"> <?php echo $title;
+?></span>
           </div>
-          
           <ul class="topbar-right">
             <li class="dropdown dropdown-profile">
               <a href="javascript:void(0)" data-toggle="dropdown">
-                <span><?php echo $conf['admin_user'];?><span class="caret"></span></span>
+                <span><?php echo $conf['admin_user'];
+?><span class="caret"></span></span>
               </a>
               <ul class="dropdown-menu dropdown-menu-right">
                 <li> <a href="./user.php"><i class="mdi mdi-lock-outline"></i> 修改密码</a> </li>
@@ -93,11 +93,7 @@ $update = update(); //检查更新
               </ul>
             </li>
           </ul>
-          
         </div>
       </nav>
-      
     </header>
     <!--End 头部信息-->
-    
- 
