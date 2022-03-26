24539c61aaeb60a59ff983251f4286c9724301b4
LyLme <admin@lylme.com>
2022-03-26 17:40:19 +0800

    修复 手机端页面适应问题

--- template/default/index.php
+++ template/default/index.php
@@ -2,6 +2,7 @@
 <html lang="zh-CN" element::-webkit-scrollbar {display:none}>
 	<head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1"/>
 		<meta charset="utf-8">
 		<title><?php echo $conf['title']?></title>
 		<meta name="keywords" content="<?php echo $conf['keywords']?>">
