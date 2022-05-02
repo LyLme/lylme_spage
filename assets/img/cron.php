9a79da251a7e80bd7b62b563694b01610a11101b
LyLme <admin@lylme.com>
2022-05-02 16:34:56 +0800

    执行CRON必须配置秘钥

--- assets/img/cron.php
+++ assets/img/cron.php
@@ -1,8 +1,43 @@
 <?php
+header("Content-Type: text/html; charset=utf-8");
+
+$pass = '';     //在这里配置密钥
+/*
+为保证安全，已禁止空密钥执行，请在 $pass 的引号内添加密钥
+CRON任务：GET  http://域名/assets/img/cron.php?key=设置的密钥
+
+例如：
+$pass = 'lylme';
+CRON地址为：http://hao.lylme.com/assets/img/cron.php?key=lylme
+
+*/
+
+
+
+
+//########以下内容可忽略########
+
+if(empty($pass)){
+    //密钥为空
+    exit('错误：禁止空密钥执行CRON，请在cron.php文件配置密钥');
+}
+else if (empty($_GET['key'])) {
+    //未传入key
+    exit('错误：密钥为空，请传入包含参数key的GET请求<br>
+    请求示例：<b>http://'.$_SERVER['HTTP_HOST'].'/assets/img/cron.php?key=秘钥</b>');
+}
+else if($pass != $_GET['key']){
+    //密钥错误
+    exit('错误：传入参数key与密钥不匹配');
+}
+else {
+   //密钥正确，执行下面代码
+
 /**
  * PHP获取bing每日壁纸
  * bing每日壁纸更新时间为UTC+8 16：00
  */
+
 if($_GET['idx']==null){
 $str=file_get_contents('http://cn.bing.com/HPImageArchive.aspx?idx=0&n=1');
 //
@@ -63,16 +98,13 @@ function GrabImage($imgurl, $dir, $filename='/background.jpg'){
 	}
 	fclose($fp2); 
 
-
-
-
 // 	echo "保存路径：" . $filename."<br>";
 	
 	return $filename; 
 }
 
 
-GrabImage($imgurl,dirname(__FILE__))
+GrabImage($imgurl,dirname(__FILE__));
 
 
 /**
@@ -87,4 +119,5 @@ GrabImage($imgurl,dirname(__FILE__))
 // }else{
 // exit('error');
 // }
+}
 ?>
