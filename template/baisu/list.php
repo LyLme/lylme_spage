c3febc2819333f9d224327f35cb03ecd5ac1ddc5
LyLme <admin@lylme.com>
2022-05-21 17:58:37 +0800

    修复 显示错误

--- template/baisu/list.php
+++ template/baisu/list.php
@@ -10,14 +10,13 @@
 // | date: 2022-05-01                                         |
 // +----------------------------------------------------------+
 session_start(); //设置session
-// 获取网站
-$groups = $DB->query("SELECT * FROM `lylme_groups` ORDER BY `group_order` ASC");
-// 获取分类
-$i = 0;
+$groups = $DB->query("SELECT * FROM `lylme_groups` ORDER BY `group_order` ASC"); // 获取分类
+
+$i = 0; //设置起始
 while ($group = $DB->fetch($groups)) {
 	//循环所有分组
 	if(!in_array($group['group_pwd'],$_SESSION['list'])&&!empty($group['group_pwd'])){
-	    echo '<script>console.log("分组跳出")</script>';
+	    //如果 分组加密未在认证列表 并且分组设置了密码(不显示分组)
 	    continue;
 	}
 	$sql = "SELECT * FROM `lylme_links` WHERE `group_id` = " . $group['group_id']." ORDER BY `link_order` ASC;";
@@ -38,27 +37,32 @@ while ($group = $DB->fetch($groups)) {
 	while ($link = $DB->fetch($group_links)) {
 		// 循环每个链接
 		// 返回指定分组下的所有字段
-
+		$lpwd = true;
 		if ($link_num > $i) {
-		    
 			$i = $i + 1;
-			if(!in_array($link['link_pwd'],$_SESSION['list'])&&!empty($link['link_pwd'])){
-	            continue;
-	        }
-			if($link["link_status"]!="0"){
-			echo "\n" . '
+			if(!empty($group['group_pwd'])&&!empty($link['link_pwd'])){
+			   //分组和链接同时加密
+			   //忽略链接加密正常显示分组
+			}
+    		else if(!in_array($link['link_pwd'],$_SESSION['list'])&&!empty($link['link_pwd'])){
+    			    //当前链接加密
+    	            $lpwd = false;
+    	   }
+    		if($link["link_status"]!="0" && $lpwd ){
+    		echo "\n" . '
             <div class="list urllist" id="id_' . $link["id"] . '" data-id="' . $link["id"] . '" data-url="' . $link["url"] . '">
-			<a rel="nofollow" href="' . $link["url"] . '" target="_blank">';
-			if ($link["icon"] == '') {
-				echo '<img src="/assets/img/default-icon.png" alt="默认' . $link["name"] . '" />';
-			} else if (!preg_match("/^<svg*/", $link["icon"])) {
-				echo '<img src="' . $link["icon"] . '" alt="' . $link["name"] . '" />';
-			} else {
-				echo $link["icon"];
-			}
-			echo	'<p class="name">'. $link["name"] . '</p><p class="desc"></p></a></div>';
-			}
+    		<a rel="nofollow" href="' . $link["url"] . '" target="_blank">';
+    		if ($link["icon"] == '') {
+    			echo '<img src="/assets/img/default-icon.png" alt="默认' . $link["name"] . '" />';
+    		} else if (!preg_match("/^<svg*/", $link["icon"])) {
+    			echo '<img src="' . $link["icon"] . '" alt="' . $link["name"] . '" />';
+    		} else {
+    			echo $link["icon"];
+    		}
+    		echo	'<p class="name">'. $link["name"] . '</p><p class="desc"></p></a></div>';
+    		}
 		}
+		
 		if ($link_num == $i) {
 			//判断当前分组链接循环完毕
 			echo '  <div class="list kongs"></div>
