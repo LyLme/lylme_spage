4a8d5cfb58174ac00fe051321a05f9d570d456ba
LyLme <admin@lylme.com>
2022-05-23 15:30:52 +0800

    增加 关于页面

---
+++ about/index.php
@@ -0,0 +1,32 @@
+<?php include("../include/common.php"); ?>
+<html lang="zh-cn">
+  <head>
+    <meta charset="UTF-8">
+    <title>关于 -  <?php echo explode("-", $conf['title'])[0];?></title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link href="https://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/docsify/4.12.2/themes/vue.min.css" type="text/css" rel="stylesheet">
+    <style>body:not(.ready){overflow:auto!important}#main{max-width:90%}p.footer{margin-top:60px}p.footer a{text-decoration:none}</style>
+ </head>
+ <body> 
+
+<div class="markdown-section" id="main">
+<?php 
+$about = 'about.txt';
+//本页内容请修改about.txt文件防止更新后index.php文件被覆盖
+
+if(file_exists($about)){
+    //文件存在，直接输出文件内容
+    echo file_get_contents($about);
+}
+else {
+    //文件不存在，从服务器获取后写入本地再输出文件
+    @file_put_contents($about,file_get_contents('https://cdn.lylme.com/lylme_spage/file/about.txt'));
+    echo file_get_contents($about);
+}
+?>
+
+<center><p class="footer"><?php echo $conf['copyright']?></p></center>
+</div>
+</body>
+</html>
---
+++ install/update5.sql
@@ -0,0 +1,2 @@
+-- v1.2.0
+INSERT INTO `lylme_tags` (`tag_id`, `tag_name`, `tag_link`, `tag_target`) VALUES (NULL, '关于本站', '/about', '1');
