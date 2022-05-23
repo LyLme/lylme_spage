3a18d4fdc84d7ba588bbe4420ea131540f7fab80
LyLme <admin@lylme.com>
2022-05-23 15:31:11 +0800

    更新 v1.2.0版本

--- install/update.sql
+++ install/update.sql
@@ -38,7 +38,7 @@ INSERT INTO `lylme_tags` (`tag_id`, `tag_name`, `tag_link`, `tag_target`) VALUES
 (1, '主页', 'https://www.lylme.com/', 0),
 (2, '博客', 'https://blog.lylme.com/', 1),
 (3, 'Github', 'https://github.com/lylme', 1),
-(4, '关于', 'https://gitee.com/LyLme/lylme_spage/blob/master/README.md', 1);
+(4, '关于本站', '/about', 1);
 ALTER TABLE `lylme_tags`
   ADD PRIMARY KEY (`tag_id`);
 ALTER TABLE `lylme_tags`
