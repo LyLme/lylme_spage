d26581c1aba04f46ca2b5e04a245a84eb5ca8c30
六零 <admin@lylme.com>
2022-06-18 20:55:17 +0800

    优化 查看页显示

--- pwd/index.php
+++ pwd/index.php
@@ -1,3 +1,6 @@
+<?php
+include("../include/common.php");
+?>
 <!DOCTYPE html>
 <html lang="zh-cn">
 <head>
@@ -5,9 +8,9 @@
 <meta name="viewport" content="width=device-width">
 <title>访问管理</title>
 <style>
-    *{margin:0;padding:0;box-sizing:border-box}
-body{text-align:center;background:#fff;color:#7b8993}
-.form-wrapper{padding-top:10%;border-radius:2px;margin:50px auto;position:relative;width:375px}
+*{margin:0;padding:0;box-sizing:border-box}
+body{text-align:center;color:#7b8993}
+.form-wrapper{padding-top:50px;border-radius:15px;margin:50px auto;position:relative;width:375px;background: #fff;}
 form{padding:30px 20px 0}
 .form-item{margin-bottom:10px;width:100%}
 .form-item input{border-radius:15px;border:1px solid #ccc;border-radius:15px;color:#000;font-size:1em;height:50px;padding:0 16px;transition:background 0.3s ease-in-out;width:100%}
@@ -24,16 +27,24 @@ and (max-width:320px){.form-wrapper{padding-top:10%;border-radius:2px;margin:50p
 .nav li.current a{background-color:#009DFF;color:#fff;padding:10px}
 .nav a{margin:5px;color:#333;text-decoration:none}
 .home{text-decoration: none;color: #bbb;line-height: 4;}
+.body{background-size: cover;display: flex;height: 100vh;align-items: center;justify-content: center;
+}
 </style>
 </head>
 <body>
+<?php
+
+if(!empty($background = background())){
+    $background = str_replace('./','../',$background);
+	echo '<div class="body" style="background-image:  url('.$background.');">';}
+?>
 <div class="form-wrapper">
-	<div class="top"><div class="colors"></div></div>
+
 	<div class="nav">
-		<ul>
-			<h1>登录</h1>
-			
-		</ul>
+
+		    <?php 
+		        if($DB->num_rows($DB->query("SELECT * FROM `lylme_pwd`"))!=0){echo '<h1>访问管理</h1>';?>
+
 	</div>
 	<?php 
 	session_start(); //设置session
@@ -66,10 +77,12 @@ and (max-width:320px){.form-wrapper{padding-top:10%;border-radius:2px;margin:50p
 		</div>
 		
 	</form>
-	<?php }?>
-	
+	<?php
+	        }
+	    }else{echo '<h2>当前站点未启用链接加密</h2>';}
+    ?>
 	<a href="../" class="home">返回首页</a>
 </div>
-
+</div>
 </body>
 </html>
