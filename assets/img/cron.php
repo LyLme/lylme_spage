828b568d38fae001b10e47404510ad39022a9130
六零 <88341678+LyLme@users.noreply.github.com>
2023-12-11 22:26:13 +0800

    Revert "修改了corn.php，使其能够获取nasa的每日一图"
    This reverts commit bdabc03cd2b10723e995b24280b479fc04d1fefd.

--- assets/img/cron.php
+++ assets/img/cron.php
@@ -1,59 +1,123 @@
 <?php
 header("Content-Type: text/html; charset=utf-8");
 
-$pass = "";     // 在这里配置密钥
-//GET  https://域名/assets/img/cron.php?key=填你的密钥
+$pass = "";     //在这里配置密钥
+/*
+为保证安全，已禁止空密钥执行，请在 $pass 的引号内添加密钥
+CRON任务：GET  http://域名/assets/img/cron.php?key=设置的密钥
 
-if (empty($pass)) {
+例如：
+$pass = 'lylme';
+CRON地址为：http://hao.lylme.com/assets/img/cron.php?key=lylme
+
+*/
+
+
+
+
+//########以下内容可忽略########
+
+if(empty($pass)){
+    //密钥为空
     exit('错误：禁止空密钥执行CRON，请在cron.php文件配置密钥');
-} else if (empty($_GET['key'])) {
+}
+else if (empty($_GET['key'])) {
+    //未传入key
     exit('错误：密钥为空，请传入包含参数key的GET请求<br>
-    请求示例：<b>https://域名/assets/img/cron.php?key=adsij' . $_SERVER['HTTP_HOST'] . '/assets/img/cron.php?key=秘钥</b>');
-} else if ($pass != $_GET['key']) {
+    请求示例：<b>http://'.$_SERVER['HTTP_HOST'].'/assets/img/cron.php?key=秘钥</b>');
+}
+else if($pass != $_GET['key']){
+    //密钥错误
     exit('错误：传入参数key与密钥不匹配');
-} else {
-    // 密钥正确，执行下面代码
+}
+else {
+   //密钥正确，执行下面代码
 
-    /**
-     * PHP下载NASA APOD每日高清图片并保存为background.jpg
-     */
+/**
+ * PHP获取bing每日壁纸
+ * bing每日壁纸更新时间为UTC+8 16：00
+ */
 
-    $api_key = '';  // 替换成你的NASA API密钥
-    $apod_url = 'https://api.nasa.gov/planetary/apod?api_key=' . $api_key;
-    $json_content = file_get_contents($apod_url);
-    $data = json_decode($json_content);
+if($_GET['idx']==null){
+$str=file_get_contents('http://cn.bing.com/HPImageArchive.aspx?idx=0&n=1');
+//
 
-    if ($data && isset($data->hdurl)) {
-        $hdurl = $data->hdurl;
+}
+else{
+    $str=file_get_contents('http://cn.bing.com/HPImageArchive.aspx?idx='.$_GET['idx'].'&n=1');
+}
 
-        echo "高清图片地址：" . $hdurl . "<br>";
+if(preg_match("/<url>(.+?)<\/url>/is",$str,$matches)){
 
-        /**
-         * 下载高清图片并保存为background.jpg
-         */
-        function DownloadAndSaveImage($imgurl, $dir, $filename = '/background.jpg')
-        {
-            if (empty($imgurl)) {
-                return false;
-            }
+if(preg_match("/<fullstartdate>(.+?)<\/fullstartdate>/is",$str,$cdata)){
+ 
+}
+$crdate = date('Y年m月d日 H:i', strtotime($cdata[1]));
+$imgurl = 'http://cn.bing.com'.$matches[1];
 
-            $dir = realpath($dir);
-            $filename = $dir . $filename;
+	echo "壁纸地址：" . $imgurl."<br>";
+	echo "发布时间：" . $crdate."<br>";
 
-            $img_content = file_get_contents($imgurl);
+}
 
-            if ($img_content !== false) {
-                file_put_contents($filename, $img_content);
-                echo "成功：高清图片已下载并保存为 " . $filename . "<br>";
-            } else {
-                echo "<p><font color='red'>错误：下载图片失败</font></p>";
-            }
-        }
 
-        DownloadAndSaveImage($hdurl, dirname(__FILE__));
-    } else {
-        echo "<p><font color='red'>错误：未能获取到高清图片地址</font></p>";
-    }
+/**
+ * 将bing每日壁纸保存到 当前目录/background.jpg
+ */
+function GrabImage($imgurl, $dir, $filename='/background.jpg'){
+	if(empty($imgurl)){
+		return false;
+	}
+	$ext = strrchr($imgurl, '.');
+	if($ext == '.js' && $ext == ".html" && $ext == ".php"){
+		echo "Format not supported！";
+		return false;
+	}
+	
+	$dir = realpath($dir);
+
+
+	$filename = $dir . $filename;
+
+	ob_start(); 
+	readfile($imgurl); 
+	$img = ob_get_contents(); 
+	ob_end_clean(); 
+	$size = strlen($img); 
+
+	$fp2 = fopen($filename , "w"); 
+	if(fwrite($fp2, $img)==true){
+	echo "壁纸大小：" . round($size / 1024) .'KB<br>';
+	
+	echo "<p><font color='green'>成功：当前壁纸已与Bing同步！</font></p>"; 
+
+	} 
+	else{
+	echo "<p><font color='red'>错误： 保存文件 <b>". $filename."</b> 失败，请检查目录权限</font></p>";
+
+	}
+	fclose($fp2); 
+
+// 	echo "保存路径：" . $filename."<br>";
+	
+	return $filename; 
+}
+
+
+GrabImage($imgurl,dirname(__FILE__));
+
+
+/**
+ * 返回bing每日壁纸
+ */
+// if($imgurl){
+// header('Content-Type: image/JPEG');
+// @ob_end_clean();
+// @readfile($imgurl);
+// @flush(); @ob_flush();
+// exit();
+// }else{
+// exit('error');
+// }
 }
 ?>
-
