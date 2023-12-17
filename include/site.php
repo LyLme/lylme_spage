844998c3e044d680113cf2acab173380bdf5e2fe
LyLme <admin@lylme.com>
2023-12-17 23:45:03 +0800

    更新 v1.8.0版本

--- include/site.php
+++ include/site.php
@@ -1,3 +1,46 @@
 <?php
 
-class site {}
+$site = new SITE($dbconfig['host'], $dbconfig['user'], $dbconfig['pwd'], $dbconfig['dbname'], $dbconfig['port']);
+class SITE extends DB
+{
+    public function __construct($db_host, $db_user, $db_pass, $db_name, $db_port)
+    {
+        parent::__construct($db_host, $db_user, $db_pass, $db_name, $db_port);
+    }
+    /**
+     * @Description 获取分组信息
+     * @return object
+     */
+    public function getGroups()
+    {
+        return $this->query("SELECT * FROM `lylme_groups` WHERE `group_pwd` = 0 ORDER BY `group_order` ASC");
+    }/**
+     * 获取分组列表
+     * @Description
+     * @return object
+     */
+    public function getCategorys()
+    {
+        //获取分组信息
+        return $this->query("SELECT * FROM `lylme_groups` WHERE `group_pwd` = 0 ORDER BY `group_order` ASC");
+    }
+    /**
+     * 获取标签菜单
+     * @Author: LyLme
+     * @return object
+     */
+    public function getTags()
+    {
+        return $this->query("SELECT * FROM `lylme_tags` ORDER BY `lylme_tags`.`sort` ASC");
+    }
+    /**
+     * 获取搜索引擎
+     * @Author: LyLme
+     * @return object
+     */
+    public function getSou()
+    {
+        return $this->query("SELECT * FROM `lylme_sou` WHERE `sou_st` = 1 ORDER BY `lylme_sou`.`sou_order` ASC");
+    }
+
+}
