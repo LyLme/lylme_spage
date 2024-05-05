9c9daf936acc769d9b3b66bd82673c2cc1e5169d
LyLme <admin@lylme.com>
2024-05-06 00:56:47 +0800

    界面优化和Bug修复

--- site/template/site.php
+++ site/template/site.php
@@ -1,12 +1,12 @@
 <?php
-/* 
- * @Description: 
+/*
+ * @Description:
  * @Author: LyLme admin@lylme.com
  * @Date: 2024-01-23 12:25:35
  * @LastEditors: LyLme admin@lylme.com
  * @LastEditTime: 2024-04-13 22:51:33
  * @FilePath: /lylme_spage/site/template/site.php
- * @Copyright (c) 2024 by LyLme, All Rights Reserved. 
+ * @Copyright (c) 2024 by LyLme, All Rights Reserved.
  */
 
 ?>
@@ -63,14 +63,14 @@
 
                         <?php
                         $tagslists = $DB->query("SELECT * FROM `lylme_tags`");
-                        while ($taglist = $DB->fetch($tagslists)) {
-                            echo '<a href="' . $taglist["tag_link"] . '"';
-                            if ($taglist["tag_target"]) {
-                                echo ' target="_blant"';
-                            }
-                            echo '><svg class="menu_icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="9773" width="32" height="32"><path d="M509.4 508.5m-469.5 0a469.5 469.5 0 1 0 939 0 469.5 469.5 0 1 0-939 0Z" fill="#242424" p-id="9774"></path><path d="M617.9 467.2c-0.3-0.6-0.5-1.2-0.8-1.8-0.1-0.1-0.1-0.2-0.2-0.4-7.2-14.5-22.7-23.9-39.9-22.6-22.5 1.8-39.4 21.5-37.6 44 0.5 5.8 2.1 11.1 4.6 15.9 11.4 25.7 6.4 57-14.6 78.1l-110 110.2c-27.3 27.3-71.7 27.3-99 0-27.3-27.3-27.3-71.7 0-99l41-41-0.3-0.3c9.5-8.2 15-20.7 14-34.1-1.8-22.5-21.5-39.4-44-37.6-10.8 0.8-20.2 5.8-27 13.2l-0.1-0.1-41.8 41.8c-59.4 59.4-59.4 155.6 0 215 59.4 59.4 155.6 59.4 215 0l110.3-110.3c46.2-46.3 56.2-114.8 30.4-171z" fill="#FFFFFF" p-id="9775"></path><path d="M762.4 257.4c-59.4-59.4-155.6-59.4-215 0L437.1 367.7c-46.2 46.2-56.2 114.7-30.5 170.9 0.3 0.6 0.5 1.2 0.8 1.8 0.1 0.1 0.1 0.2 0.2 0.4 7.2 14.5 22.7 23.9 39.9 22.6 22.5-1.8 39.4-21.5 37.6-44-0.5-5.8-2.1-11.1-4.6-15.9-11.4-25.7-6.4-57 14.6-78.1l110.1-110.1c27.3-27.3 71.7-27.3 99 0 27.3 27.3 27.3 71.7 0 99l-41 41 0.3 0.3c-9.5 8.2-15 20.7-14 34.1 1.8 22.5 21.5 39.4 44 37.6 10.8-0.8 20.2-5.8 27-13.2l0.1 0.1 41.8-41.8c59.3-59.4 59.3-155.7 0-215z" fill="#FFFFFF" p-id="9776"></path></svg> ' . $taglist["tag_name"] . '</a>
+while ($taglist = $DB->fetch($tagslists)) {
+    echo '<a href="' . $taglist["tag_link"] . '"';
+    if ($taglist["tag_target"]) {
+        echo ' target="_blant"';
+    }
+    echo '><svg class="menu_icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="9773" width="32" height="32"><path d="M509.4 508.5m-469.5 0a469.5 469.5 0 1 0 939 0 469.5 469.5 0 1 0-939 0Z" fill="#242424" p-id="9774"></path><path d="M617.9 467.2c-0.3-0.6-0.5-1.2-0.8-1.8-0.1-0.1-0.1-0.2-0.2-0.4-7.2-14.5-22.7-23.9-39.9-22.6-22.5 1.8-39.4 21.5-37.6 44 0.5 5.8 2.1 11.1 4.6 15.9 11.4 25.7 6.4 57-14.6 78.1l-110 110.2c-27.3 27.3-71.7 27.3-99 0-27.3-27.3-27.3-71.7 0-99l41-41-0.3-0.3c9.5-8.2 15-20.7 14-34.1-1.8-22.5-21.5-39.4-44-37.6-10.8 0.8-20.2 5.8-27 13.2l-0.1-0.1-41.8 41.8c-59.4 59.4-59.4 155.6 0 215 59.4 59.4 155.6 59.4 215 0l110.3-110.3c46.2-46.3 56.2-114.8 30.4-171z" fill="#FFFFFF" p-id="9775"></path><path d="M762.4 257.4c-59.4-59.4-155.6-59.4-215 0L437.1 367.7c-46.2 46.2-56.2 114.7-30.5 170.9 0.3 0.6 0.5 1.2 0.8 1.8 0.1 0.1 0.1 0.2 0.2 0.4 7.2 14.5 22.7 23.9 39.9 22.6 22.5-1.8 39.4-21.5 37.6-44-0.5-5.8-2.1-11.1-4.6-15.9-11.4-25.7-6.4-57 14.6-78.1l110.1-110.1c27.3-27.3 71.7-27.3 99 0 27.3 27.3 27.3 71.7 0 99l-41 41 0.3 0.3c-9.5 8.2-15 20.7-14 34.1 1.8 22.5 21.5 39.4 44 37.6 10.8-0.8 20.2-5.8 27-13.2l0.1 0.1 41.8-41.8c59.3-59.4 59.3-155.7 0-215z" fill="#FFFFFF" p-id="9776"></path></svg> ' . $taglist["tag_name"] . '</a>
                             ' . "\n";
-                        } ?>
+} ?>
                     </div>
                 </div>
                 <img class="night-mode-btn" src="/site/static/light_mode.svg" alt="切换模式" onclick="toggleNightMode()">
@@ -94,9 +94,9 @@
                         <!-- <i class="fa fa-chrome"></i> -->
                         <a class="path_tag" href="/">首页</a>
                         <?php
-                        echo '<i class="fa fa-angle-right pathi"></i> <a class="path_tag">' . $group_name . '</a>';
-                        echo '<i class="fa fa-angle-right pathi"></i> <span class="path_tag">'  . $url_name . '</span>';
-                        ?>
+echo '<i class="fa fa-angle-right pathi"></i> <a class="path_tag">' . $group_name . '</a>';
+echo '<i class="fa fa-angle-right pathi"></i> <span class="path_tag">'  . $url_name . '</span>';
+?>
 
 
                     </div>
@@ -122,12 +122,12 @@
                             </div>
                             <div class="urls-tools-item urls-tools-qr">
                                 <a href="javascript:void(0);" class="urls-tools-btn btn-wap">手机查看 <i class="fa fa-qrcode"></i></a>
-                                <span id="code"><img width="100" height="100" src="/include/qrcode.php?text=<?php $url_herf ?>"></span>
+                                <span id="code"><img width="100" height="100" src="/include/qrcode.php?text=<?php echo $url_herf ?>"></span>
                             </div>
                         </div>
                         <?php
-                        if (!empty($conf['snapshot'])) {
-                        ?>
+if (!empty($conf['snapshot'])) {
+    ?>
                             <div class="site-right-column">
                                 <h4>网页快照</h4>
                                 <div class="site-left-info site-info view_img">
@@ -140,7 +140,7 @@
                                 });
                             </script>
                         <?php
-                        } ?>
+} ?>
 
                     </div>
 
@@ -159,7 +159,7 @@
                 <?php if (!empty($conf['wztj'])) {
                     echo '<p>' . $conf["wztj"] . '</p>';
                 }
-                ?>
+?>
                 <p> <?php echo $conf['copyright']; ?> </p>
 
 
