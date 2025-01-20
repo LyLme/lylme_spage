14f5f1c48ba727834e56161928b42ff891deb36b
LyLme <admin@lylme.com>
2025-01-21 00:22:55 +0800

    增加热榜

--- template/dashlite/config.php
+++ template/dashlite/config.php
@@ -9,5 +9,24 @@ $theme_config =  [
     'value' => 'https://cn.bing.com/search?q=%s',
     'placeholder' => "https://cn.bing.com/search?q="
 
+  ],
+  [
+    'type' => 'select',
+    'name' => 'lytoday',
+    'title' => '今日热榜',
+    'description' => 'LyToday-JS插件显示位置，每日免费请求上限200次 <a href="https://doc.lylme.com/spage/#/lytoday-js" target="_blank">查看文档</a>',
+    "value" => 0,
+    'enum' => [
+      0 => "关闭",
+      1 => "底部"
+    ],
+
+  ],
+  [
+    'type' => 'textarea',
+    'name' => 'lytodaycode',
+    'title' => '今日热榜代码',
+    'description' => 'LyToday-JS插件自定义代码，若不了解请勿修改 <a href="https://doc.lylme.com/spage/#/lytoday-js" target="_blank">查看文档</a>',
+    'value' => '<div id="lytoday"></div><script src="https://lytoday.lylme.com/"></script>',
   ]
 ];
