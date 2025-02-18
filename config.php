ae11c514e0eb2a15fbcabcd13603fa9841c6f810
LyLme <admin@lylme.com>
2025-02-19 00:19:30 +0800

    checkoutfile

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
-        /*数据库配置*/
-        $dbconfig=array(
-            "host" => "localhost", //数据库服务器
-            "port" => 3306, //数据库端口
-            "user" => "lylmespage", //数据库用户名
-            "pwd" => "lylmespage", //数据库密码
-            "dbname" => "lylmespage", //数据库名
-        );
-        ?>
+/*数据库配置*/
+$dbconfig=array(
+	"host" => "localhost", //数据库服务器
+	"port" => 3306, //数据库端口
+	"user" => "", //数据库用户名
+	"pwd" => "", //数据库密码
+	"dbname" => "", //数据库名
+);
+?>
