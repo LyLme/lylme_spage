472fb5d5b6d4df33d27b2672d69b172bcdefafb1
LyLme <admin@lylme.com>
2025-02-18 23:36:50 +0800

    错误修复

--- install/templates/step_4.php
+++ install/templates/step_4.php
@@ -25,7 +25,7 @@
                 <p><b>后台地址：</b><a href="<?php echo $admin_url; ?>"><?php echo $admin_url; ?></a></p>
                 <p><b>后台账号：</b>admin</p>
                 <p><b>后台密码：</b>123456</p><br /><br />
-                您可以访问 <a href="https://doc.lylme.com/sapge/" target="_blank" class="link">帮助文档</a> 获取更多帮助
+                您可以访问 <a href="https://doc.lylme.com/spage/#/readme" target="_blank" class="link">帮助文档</a> 获取更多帮助
             </div>
         </div>
     </div>
