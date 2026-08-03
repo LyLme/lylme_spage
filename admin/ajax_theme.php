8452db46060123c35766e93245590a82ed4ae429
Neko <69409093+neko205-mx@users.noreply.github.com>
2026-08-03 21:33:18 +0800

    代码逻辑问题导致的RCE修复 (#132)

--- admin/ajax_theme.php
+++ admin/ajax_theme.php
@@ -10,9 +10,8 @@
  */
 
 include_once("../include/common.php");
-if (isset($islogin) == 1) {
-} else {
-    exit("<script language='javascript'>window.location.href='./login.php';</script>");
+if (!isset($islogin) || $islogin !== 1) {
+    exit("<script>window.location.href='./login.php';</script>");
 }
 header('Content-Type:application/json');
 
